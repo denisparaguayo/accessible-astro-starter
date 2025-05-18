52f2116344aa3e7274aeeec087186e45b725d395
Denis Cantero <denis.cantero1987@gmail.com>
2025-05-18 01:58:43 -0300

    agregando site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import path from 'path'
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
-  site: 'https://accessible-astro-starter.incluud.dev',
+  site: 'https://formuleros.netlify.app/',
   integrations: [mdx(), icon(), compress(), react()], // Agrega 'react()' a las integraciones
   vite: {
     css: {
